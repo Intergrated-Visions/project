de1d5ac2292cde6f9a90b7cab1310952e4fcd9bd
harcyrus <cyrusharding604@gmail.com>
2024-03-23 16:49:57 -0400

    new scripts

--- scripts/tailscale.sh
+++ scripts/tailscale.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Tailscale login script
-
+curl -fsSL https://tailscale.com/install.sh | sh
 sudo tailscale up --authkey tskey-auth-kpKpbBokci11CNTRL-t6JBXQcA6gBKkGD3wSPbgB79o1EXra98
