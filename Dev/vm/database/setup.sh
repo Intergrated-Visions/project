0524a96df19cef3b0522bf57e601654ee4cc549e
YardleyFra <francoisyardley@gmail.com>
2024-04-06 22:42:59 -0400

    automatedsystem

--- Dev/vm/database/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Update package index
-sudo apt update
-
-# Install PHP CLI, PHP MySQL extension, and PHP AMQP extension
-sudo apt install -y php-cli php-amqp
--- QA/frontend/website/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Expire the session cookie by setting its expiration time to a past value
-setcookie("session", '', time() - 3600);
-
-// Redirect to the login page or any other desired page
-header("Location: login.php");
-exit;
-?>
