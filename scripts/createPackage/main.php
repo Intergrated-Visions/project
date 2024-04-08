ba1052a6dd5c70b3ddb76cbedcff07026d93f6f2
YardleyFra <francoisyardley@gmail.com>
2024-04-08 18:54:07 -0400

    filerepair

--- scripts/createPackage/main.php
+++ scripts/createPackage/main.php
@@ -1,2 +1,18 @@
 <?php
-<?
+require_once('rabbitMQLib.inc');
+
+$connectionConfig = [
+    "BROKER_HOST" => "$BROKER_HOST", // ! TO CHANGE
+    "BROKER_PORT" => 5672,
+    "USER" => "test",  // ! TO CHANGE
+    "PASSWORD" => "test",  // ! TO CHANGE
+    "VHOST" => "integratedVisions",  // ! TO CHANGE
+];
+
+$exchangeQueueConfig = [
+    "EXCHANGE_TYPE" => "topic",
+    "AUTO_DELETE" => true,
+    "EXCHANGE" => "authenticationExchange", // ! TO CHANGE
+    "QUEUE" => "authenticationQueue" // ! TO CHANGE
+];
+
