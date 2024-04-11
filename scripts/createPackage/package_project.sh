87dc3e5c76b59ccfabef5cc0f28cba25a77925e4
YardleyFra <francoisyardley@gmail.com>
2024-04-10 21:40:44 -0400

    DB

--- scripts/createPackage/package_project.sh
+++ scripts/createPackage/package_project.sh
@@ -6,24 +6,32 @@ projectDir="/home/yardley/Desktop/project"
 # The directory where the zip file will be saved
 zipSaveDir="/home/yardley/Desktop/project/packages"
 
-# The name of the archive you want to create
-zipFileName="project_backup_$(date +%Y-%m-%d_%H-%M-%S).zip"
-
 # Ensure the save directory exists
 mkdir -p "$zipSaveDir"
 
-# Navigate to the project directory to use relative paths for exclusions
+# Navigate to the project directory to use relative paths
 cd "$projectDir" || exit
 
-# Package the project into a zip file, excluding the 'packages' folder and '.git' folder
-# -r: recursively include files in directories
-# -9: use the maximum compression level
-# -x: exclude the following patterns
-zip -r -9 "$zipSaveDir/$zipFileName" . -x "packages/*" ".git/*"
+# The name of the archive you want to create
+zipFileName="project_backup_$(date +%Y-%m-%d_%H-%M-%S).zip"
+
+# Package the project into a zip file, excluding the '.git' folder and the packages directory itself to avoid recursion
+zip -r -9 "$zipSaveDir/$zipFileName" . -x ".git/*" "packages/*"
 
 # Check if the ZIP command succeeded
 if [ $? -eq 0 ]; then
     echo "Package has been created successfully: $zipSaveDir/$zipFileName"
 else
     echo "Could not create the zip package."
+    exit 1
+fi
+
+# Check the number of backup packages in the directory and delete the oldest if more than 3
+packageCount=$(ls -1 "$zipSaveDir"/*.zip 2>/dev/null | wc -l)
+
+if [ "$packageCount" -gt 3 ]; then
+    echo "More than 3 backup packages found. Deleting the oldest backup package..."
+    oldestPackage=$(ls -t "$zipSaveDir"/*.zip | tail -1)
+    rm -f "$oldestPackage"
+    echo "Deleted the oldest package: $oldestPackage"
 fi
