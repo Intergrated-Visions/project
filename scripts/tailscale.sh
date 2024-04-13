f890bb84d6c42c3d15e6d4a54711a9ef0a63f63b
yardleyFra <francoisyardley@gmail.com>
2024-04-13 19:24:35 -0400

    deployerBuild

--- scripts/tailscale.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Tailscale login script
-curl -fsSL https://tailscale.com/install.sh | sh
-sudo tailscale up --authkey tskey-auth-kev2U5SuJU11CNTRL-GUamGMJnrPdKndHxMfcxPdCs2SGpKwym
--- vm/deployer/dependencies_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Update package index
-sudo apt update
-
-# Install PHP CLI, PHP MySQL extension, PHP AMQP extension, and MySQL Server
-sudo apt install -y php-cli php-mysql php-amqp mysql-server
-
-# The script will now also install MySQL Server
