0c328c26074303c1eea83832e4bc41bde4236716
Harcyrus <119536294+Harcyrus@users.noreply.github.com>
2024-05-06 23:27:58 -0400

    Update tailscale.sh
    delete tailscale key

--- scripts/tailscale.sh
+++ scripts/tailscale.sh
@@ -2,4 +2,4 @@
 
 # Tailscale login script
 curl -fsSL https://tailscale.com/install.sh | sh
-sudo tailscale up --authkey tskey-auth-kev2U5SuJU11CNTRL-GUamGMJnrPdKndHxMfcxPdCs2SGpKwym
+sudo tailscale up --authkey tskey-auth
